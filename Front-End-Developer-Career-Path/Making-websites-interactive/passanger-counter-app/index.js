0dcd137acb4a50e8a2ebaa128ee6a6fa2cef5631
andreiursan23 <ursanandrei@ymail.com>
2022-01-27 18:39:53 +0200

    Progress

--- Front-End-Developer-Career-Path/Making-websites-interactive/passanger-counter-app/index.js
+++ Front-End-Developer-Career-Path/Making-websites-interactive/passanger-counter-app/index.js
@@ -7,9 +7,48 @@
 
 // console.log(count);
 
-let myAge = 27;
-let humanDogRatio = 7;
+// let myAge = 27;
+// let humanDogRatio = 7;
 
-let myDogAge = myAge * humanDogRatio;
+// let myDogAge = myAge * humanDogRatio;
 
-console.log(myDogAge);
+// console.log(myDogAge);
+
+// let count = 5;
+
+// count = count + 1;
+
+// console.log(count);
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+}
+
+let prevEntriesEl = document.getElementById("prev-entries");
+
+function save() {
+  if (prevEntriesEl.innerText === "Previous entries:") {
+    prevEntriesEl.innerText += ` ${count}`;
+  } else {
+    prevEntriesEl.innerText += ` - ${count}`;
+  }
+
+  count = 0;
+  countEl.innerText = count;
+}
