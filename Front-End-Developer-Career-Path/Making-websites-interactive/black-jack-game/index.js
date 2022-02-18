0d7a9269099d33b0c1e984e7fc1502f964d7cacb
andreiursan23 <ursanandrei@ymail.com>
2022-02-18 19:36:15 +0200

    Fixed minor bugs

--- Front-End-Developer-Career-Path/Making-websites-interactive/black-jack-game/index.js
+++ Front-End-Developer-Career-Path/Making-websites-interactive/black-jack-game/index.js
@@ -20,25 +20,6 @@ let hasPlayerWon = false;
 let hasDealerWon = false;
 let hasPlayerAce = false;
 let hasDealerAce = false;
-let gameStage = 1;
-
-/*
-  1. Casa si jucatorul primesc cate 2 carti
-    1.1 Casa are doar a doua carte vizibila
-    1.2 Jucatorul are ambele carti vizibile
-  2. Jucatorul isi joaca tura
-    2.1 Se poate opri dupa primirea celor 2 carti initiale
-    2.2 Poate cere sa primeasca alte carti
-      2.2.1 Daca suma cartilor depaseste 21, jucatorul pierde instant runda
-    2.3 Cand este multumit cu suma cartilor pe care le are, apasa Stand
-  3. Este aratata a doua carte a casei
-  4. Daca suma cartilor casei este sub 17 este obligata sa mai traga o carte
-    4.1 Daca suma cartilor casei depaseste 21, jucatorul este declarat automat castigator
-  5. Se evalueaza cele doua suma si se stabileste castigatorul rundei
-
-
-  Daca am 1, atunci initial apare ca
-*/
 
 startGameBtn.addEventListener("click", startGame);
 newCardBtn.addEventListener("click", getNewCard);
@@ -128,10 +109,14 @@ function dealerGame() {
   if (dealersSum < 17) {
     dealersCards.push(generateRandomCard("dealer"));
   }
+
+  let sum = calcCardsSum(dealersCards);
+
+  updateCardsSum("dealer", sum);
 }
 
 function evaluateGameState(sideCardsSum) {
-  if (sideCardsSum < 21) {
+  if (sideCardsSum <= 21) {
     return true;
   } else {
     return false;
@@ -158,6 +143,14 @@ function updateGameMessage(message) {
   gameStatusEl.textContent = `${message}`;
 }
 
+function endGameSteps() {
+  renderGame(dealersCards, dealersCardsValuesEl, dealersCardsSumEl, "dealer");
+
+  startGameBtn.disabled = false;
+  newCardBtn.disabled = true;
+  standBtn.disabled = true;
+}
+
 function startGame() {
   dealersCards = [generateRandomCard("dealer"), generateRandomCard("dealer")];
   playerCards = [generateRandomCard("player"), generateRandomCard("player")];
@@ -174,18 +167,12 @@ function startGame() {
 
   if (hasPlayerWon && hasDealerWon) {
     updateGameMessage("Both sides have Blackjack! It's a tie. 😲");
-    renderGame(dealersCards, dealersCardsValuesEl, dealersCardsSumEl, "dealer");
 
-    startGameBtn.disabled = false;
-    newCardBtn.disabled = true;
-    standBtn.disabled = true;
+    endGameSteps();
   } else if (hasPlayerWon) {
     updateGameMessage("Blackjack! You won this round! 🥇");
-    renderGame(dealersCards, dealersCardsValuesEl, dealersCardsSumEl, "dealer");
 
-    startGameBtn.disabled = false;
-    newCardBtn.disabled = true;
-    standBtn.disabled = true;
+    endGameSteps();
   } else {
     updateGameMessage("Draw a new card? Or stand down? 🤔");
   }
@@ -199,11 +186,8 @@ function getNewCard() {
 
   if (!isPlayerAlive) {
     updateGameMessage("You lost this round! 😢");
-    renderGame(dealersCards, dealersCardsValuesEl, dealersCardsSumEl, "dealer");
 
-    startGameBtn.disabled = false;
-    newCardBtn.disabled = true;
-    standBtn.disabled = true;
+    endGameSteps();
   }
 }
 
@@ -214,17 +198,11 @@ function standRound() {
 
   if (hasPlayerWon) {
     updateGameMessage("You won this round! 🥇");
-    renderGame(dealersCards, dealersCardsValuesEl, dealersCardsSumEl, "dealer");
 
-    startGameBtn.disabled = false;
-    newCardBtn.disabled = true;
-    standBtn.disabled = true;
+    endGameSteps();
   } else {
     updateGameMessage("You lost this round! 😢");
-    renderGame(dealersCards, dealersCardsValuesEl, dealersCardsSumEl, "dealer");
 
-    startGameBtn.disabled = false;
-    newCardBtn.disabled = true;
-    standBtn.disabled = true;
+    endGameSteps();
   }
 }
